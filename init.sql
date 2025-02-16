130a1ba0b88535da96e126ed89598ca5919c4de8
kangminhyuk1111 <rkdglqkr12@naver.com>
2025-02-17 08:53:54 +0900

    feat: init.sql 수정

--- init.sql
+++ init.sql
@@ -118,3 +118,17 @@ CREATE TABLE `match_participation` (
     CONSTRAINT `match_participation_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE,
     CONSTRAINT `match_participation_ibfk_2` FOREIGN KEY (`matching_id`) REFERENCES `match` (`id`) ON DELETE CASCADE)
     ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+ALTER TABLE `match`
+    DROP FOREIGN KEY `match_ibfk_1`,
+    ADD CONSTRAINT `fk_match_host_cascade`  -- 새로운 이름으로 변경
+        FOREIGN KEY (`host_id`)
+            REFERENCES `users` (`id`)
+            ON DELETE CASCADE;
+
+ALTER TABLE `court_reviews`
+    DROP FOREIGN KEY `fk_review_user`,
+    ADD CONSTRAINT `fk_review_user_cascade`
+        FOREIGN KEY (`user_id`)
+            REFERENCES `users` (`id`)
+            ON DELETE CASCADE;
